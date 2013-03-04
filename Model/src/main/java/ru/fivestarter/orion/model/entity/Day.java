7b0ec36ce829ee965c4d88a8a9ba3defc8f7bdd8
fivestarter <fivestarter@gmail.com>
2013-03-04 14:38:42 +0400

    entities were added.

--- Model/src/main/java/ru/fivestarter/orion/model/entity/Day.java
+++ Model/src/main/java/ru/fivestarter/orion/model/entity/Day.java
@@ -1,8 +1,11 @@
 package ru.fivestarter.orion.model.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import ru.fivestarter.orion.model.auth.entity.User;
 import ru.fivestarter.orion.model.common.BaseEntity;
 
 /**
@@ -14,4 +17,15 @@ import ru.fivestarter.orion.model.common.BaseEntity;
 public class Day extends BaseEntity{
     private static final long serialVersionUID = 1L;
 
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false, updatable = false, insertable = false)
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- Model/src/main/java/ru/fivestarter/orion/model/entity/Purpose.java
+++ Model/src/main/java/ru/fivestarter/orion/model/entity/Purpose.java
@@ -1,6 +1,9 @@
 package ru.fivestarter.orion.model.entity;
 
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import ru.fivestarter.orion.model.common.BaseEntity;
@@ -14,4 +17,14 @@ import ru.fivestarter.orion.model.common.BaseEntity;
 public class Purpose extends BaseEntity{
     private static final long serialVersionUID = 1L;
 
+    @OneToMany(orphanRemoval = true)
+    private List<Purpose> children;
+
+    public List<Purpose> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Purpose> children) {
+        this.children = children;
+    }
 }
